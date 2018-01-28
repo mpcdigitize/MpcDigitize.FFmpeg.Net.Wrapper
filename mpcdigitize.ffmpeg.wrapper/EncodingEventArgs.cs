d8ec4a84ef0f673ccdd71d25211f7881c78b2af2
mpcdigitize <plawrow@hotmail.com>
2018-01-28 12:53:30 -0500

    add EncodingEventArgs

---
+++ mpcdigitize.ffmpeg.wrapper/EncodingEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpcdigitize.ffmpeg.wrapper
+{
+    public class EncodingEventArgs
+    {
+
+        public string Duration { get; set; }
+        public double Progress { get; set; }
+        public string InputFile { get; set; }
+        public string OutputFile { get; set; }
+    }
+}
