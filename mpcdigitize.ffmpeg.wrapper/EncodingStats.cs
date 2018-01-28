b9c55cb975ad8ea8c5ca0a1b64ea643a77e20c93
mpcdigitize <plawrow@hotmail.com>
2018-01-28 12:52:08 -0500

    add EncodingStats class

---
+++ mpcdigitize.ffmpeg.wrapper/EncodingStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpcdigitize.ffmpeg.wrapper
+{
+    public class EncodingStats
+    {
+        public string Duration { get; set; }
+        public double Progress { get; set; }
+
+    }
+}
