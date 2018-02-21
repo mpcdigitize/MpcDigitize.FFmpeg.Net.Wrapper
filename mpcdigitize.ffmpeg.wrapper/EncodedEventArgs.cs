109a3a3283f31fa72e41e309c73301fe05ea007f
mpcdigitize <plawrow@hotmail.com>
2018-02-21 16:37:10 +0000

    create EncodedEventArgs.cs

--- mpcdigitize.ffmpeg.wrapper/EncodedEventArgs.cs
+++ mpcdigitize.ffmpeg.wrapper/EncodedEventArgs.cs
@@ -1,1 +1,18 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpcdigitize.ffmpeg.wrapper
+{
+    public class EncodedEventArgs: EventArgs
+    {
+               
+        public EncodingJob EncodingJob {get;set;}
+       
+     
+
+    }
+}
