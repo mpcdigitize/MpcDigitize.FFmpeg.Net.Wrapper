8ba5a2b408ed8b7d74c9cac73ba01a937aaa84a3
mpcdigitize <plawrow@hotmail.com>
2018-02-21 22:07:50 +0000

    updade Streams enum

--- mpcdigitize.ffmpeg.wrapper/Enums/Streams.cs
+++ mpcdigitize.ffmpeg.wrapper/Enums/Streams.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mpcdigitize.Ffmpeg.Wrapper.Enums
+{
+    public enum Streams
+    {
+        AudioStream,
+        VideoStream,
+        AllStreams,
+        
+
+
+    }
+
+}
 
