d86789006f80a45a122c68a8d7cd6eb4b74dd81d
mpcdigitize <plawrow@hotmail.com>
2018-02-21 18:39:20 +0000

    update AudioArgs

--- mpcdigitize.ffmpeg.wrapper/AudioArgs.cs
+++ mpcdigitize.ffmpeg.wrapper/AudioArgs.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mpcdigitize.Ffmpeg.Wrapper
+{
+    public class AudioArgs : EncodingArgs
+    {
+        
+        private Dictionary<string, string> _arguments;
+        
+        public AudioArgs()
+        {
+          _arguments = this.Arguments
+      
+        }
+    
+       
+        
+
+
+
+     
+    }
+}
 
