1d94e845a74bae183ded3f1f2fc93b079912faf7
mpcdigitize <plawrow@hotmail.com>
2018-02-21 18:37:48 +0000

    update VideoArgs.cs

--- mpcdigitize.ffmpeg.wrapper/VideoArgs.cs
+++ mpcdigitize.ffmpeg.wrapper/VideoArgs.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Mpcdigitize.Ffmpeg.Wrapper
+{
+    public class VideoArgs : EncodingArgs
+    {
+        
+        private Dictionary<string, string> _arguments;
+        
+        public VideoArgs()
+        {
+          _arguments = this.Arguments
+      
+        }
+    
+       
+        
+
+
+
+     
+    }
+}
