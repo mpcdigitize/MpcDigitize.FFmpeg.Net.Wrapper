258a69869a2c1d2b7ce39cf62ca6b7970cf38da8
mpcdigitize <plawrow@hotmail.com>
2018-02-23 20:02:05 +0000

    Bitrate.cs,, change namespace

--- mpcdigitize.ffmpeg.wrapper/Enums/Bitrate.cs
+++ mpcdigitize.ffmpeg.wrapper/Enums/Bitrate.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mpcdigitize.Ffmpeg.Wrapper.Enums
+namespace MpcDigitize.FFmpeg.Net.Wrapper
 {
 
     public enum Bitrate
