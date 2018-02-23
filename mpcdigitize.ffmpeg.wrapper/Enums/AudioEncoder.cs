888517a2da30dd68cb6405ff62cf181e691195d1
mpcdigitize <plawrow@hotmail.com>
2018-02-23 20:00:37 +0000

    AudioEncoder.cs, change namespace

--- mpcdigitize.ffmpeg.wrapper/Enums/AudioEncoder.cs
+++ mpcdigitize.ffmpeg.wrapper/Enums/AudioEncoder.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mpcdigitize.Ffmpeg.Wrapper.Enums
+namespace MpcDigitize.FFmpeg.Net.Wrapper
 {
     public enum AudioEncoder
     {
