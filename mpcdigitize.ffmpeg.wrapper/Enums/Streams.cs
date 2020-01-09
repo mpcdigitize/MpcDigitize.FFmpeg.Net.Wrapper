2bd8bc9a8067f1fe84895cc93b3f24b09c167781
mpcdigitize <plawrow@hotmail.com>
2020-01-08 21:30:20 -0500

    added stream arguments for extracting audio and video stream

--- mpcdigitize.ffmpeg.wrapper/Enums/Streams.cs
+++ mpcdigitize.ffmpeg.wrapper/Enums/Streams.cs
@@ -11,7 +11,20 @@ namespace MpcDigitize.FFmpeg.Net.Wrapper
         AudioStream,
         VideoStream,
         AllStreams,
-        
+        AudioStream0,
+        AudioStream1,
+        AudioStream2,
+        AudioStream3,
+        AudioStream4,
+        AudioStream5,
+        VideoStream0,
+        VideoStream1,
+        VideoStream2,
+        VideoStream3,
+        VideoStream4,
+        VideoStream5
+
+
 
 
     }
